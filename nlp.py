e096419be6506680b0963967886b140c47a364af
Uneto47 <netovasconcelos02@gmail.com>
2024-08-03 01:07:21 -0300

    fix: embeddings generate

--- nlp.py
+++ nlp.py
@@ -3,24 +3,33 @@ import requests
 import pandas as pd
 from chromadb import Client
 from dotenv import load_dotenv
+import torch.nn.functional as F
+from torch import Tensor
+from transformers import AutoTokenizer, AutoModel
+
 load_dotenv()
 
+def average_pool(last_hidden_states: Tensor, attention_mask: Tensor) -> Tensor:
+    last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
+    return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
 
-API_URL = "https://api-inference.huggingface.co/models/intfloat/multilingual-e5-small"
 HUGGINGFACE_TOKEN = os.getenv("TOKEN")
 DATA_URL = "https://olinda.bcb.gov.br/olinda/servico/mecir_dinheiro_em_circulacao/versao/v1/odata/informacoes_diarias_com_categoria?$top=500&$format=json"
 
-def get_embeddings(sentences, source):
-    headers = {"Authorization": f"Bearer {HUGGINGFACE_TOKEN}"}
-    payload = {"inputs": {"source_sentence": source, "sentences": sentences}}
-    response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
+def get_embeddings(sentences):
+    tokenizer = AutoTokenizer.from_pretrained('intfloat/multilingual-e5-small')
+    model = AutoModel.from_pretrained('intfloat/multilingual-e5-small')
+    batch_dict = tokenizer(sentences, max_length=512, padding=True, truncation=True, return_tensors='pt')
+    outputs = model(**batch_dict)
+    embeddings = average_pool(outputs.last_hidden_state, batch_dict['attention_mask']) # type: ignore
+    embeddings = F.normalize(embeddings, p=2, dim=1)  # Normalize embeddings
+    return embeddings
 
 def fetch_data():
     response = requests.get(DATA_URL)
     data = response.json()
     df = pd.DataFrame(data['value'])
-    columns = ['Data', 'Valor', 'Quantidade', 'Categoria', 'Denominacao', 'Especie']
+    columns = ['Data', 'Valor', 'Categoria', 'Denominacao']
     df = df[columns]
     sentences = df.apply(create_sentences, axis=1).tolist()
     return sentences
@@ -32,25 +41,27 @@ def init_chromadb():
 
 def add_embeddings_to_chromadb(collection, sentences, embeddings):
     for i, (sentence, embedding) in enumerate(zip(sentences, embeddings)):
-        collection.add(documents=[sentence], embeddings=[embedding], ids=[f"id_{i}"])
+        embedding_list = embedding.tolist()
+        collection.add(documents=[sentence], embeddings=[embedding_list], ids=[f"id_{i}"])
 
 def create_sentences(row):
-    return f"Data: {row['Data']}, Valor: {row['Valor']}, Quantidade: {row['Quantidade']}, Categoria: {row['Categoria']}, Denominacao: {row['Denominacao']}, Especie: {row['Especie']}"
+    return f"query: Data: {row['Data']}, Valor: {row['Valor']}, Categoria: {row['Categoria']}, Denominacao: {row['Denominacao']}"
  
 def main():
     sentences = fetch_data()
 
-    query_sentence = input("Digite a consulta: ")
+    query_sentence = "query: Futebol"
     
-    
-    embeddings = get_embeddings(sentences, query_sentence)
+    embeddings = get_embeddings(sentences)
     
     collection = init_chromadb()
     
     add_embeddings_to_chromadb(collection, sentences, embeddings)
     
+    query_embedding = get_embeddings([query_sentence]).tolist()[0]  # Gerar embedding da consulta e converter para lista
+    
     results = collection.query(
-        query_embeddings=[1.0],
+        query_embeddings=[query_embedding],
         n_results=3
     )
     
