5f0bbc3ba17f14a48c72c15219a3f5e5aed274e8
Uneto47 <netovasconcelos02@gmail.com>
2024-08-04 10:18:26 -0300

    chore: remove unused imports

--- embedding_utils.py
+++ embedding_utils.py
@@ -1,8 +1,5 @@
-import torch.nn.functional as F
-from torch import Tensor
 from sentence_transformers import SentenceTransformer
 
-# Carregar o modelo
 def get_embeddings(sentences):
     model = SentenceTransformer('intfloat/multilingual-e5-small')
     return model.encode(sentences, convert_to_tensor=False)
